79204b1f96dfc4daef64833c94ef703758cc0212
DDemmer1 <Dennis.Demmer@web.de>
2019-10-11 14:51:28 +0200

    add wuerstchenizes now every 2 seconds for dynamic content

--- content.js
+++ content.js
@@ -1,18 +1,28 @@
-var elements = document.getElementsByTagName('*');
+//for static content
+wuerstchenize();
 
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
+//for dynamically loaded content
+setInterval(function () {
+    wuerstchenize();
+}, 2000);
 
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
+function wuerstchenize(){
+    var elements = document.getElementsByTagName('*');
 
-        if (node.nodeType === 3) {
-            var text = node.nodeValue;
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
 
-            var replacedText = text.replace(/Terror|Wolf|Terrorist|Attentäter|Täter|Attentat|Täter|Attentat|Anschlag|Todesschütze|Wölfe/gi, 'Würstchen');
+        for (var j = 0; j < element.childNodes.length; j++) {
+            var node = element.childNodes[j];
 
-            if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
+            if (node.nodeType === 3) {
+                var text = node.nodeValue;
+
+                var replacedText = text.replace(/Terror|Wolf|Terrorist|Attentäter|Täter|Attentat|Täter|Attentat|Anschlag|Todesschütze|Wölfe/gi, 'Würstchen');
+
+                if (replacedText !== text) {
+                    element.replaceChild(document.createTextNode(replacedText), node);
+                }
             }
         }
     }
